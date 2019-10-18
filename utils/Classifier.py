cab4b5821afb5cd9f83099e4be6ba25403ef33f4
Wenhan Ji <henryji96@outlook.com>
2019-10-17 22:38:28 -0400

    Update code for cv

--- utils/Classifier.py
+++ utils/Classifier.py
@@ -176,37 +176,73 @@ class Classifier(object):
         return clf, importance, score_df
 
 
+    def grid_search_cv(X, y, base_learner, param_grid, n_splits=10):
+        '''
+        scoring:
+            supported scorers: sorted(sklearn.metrics.SCORERS.keys())
+            if input a list, refit/best_estimator is not supported
+        '''
+        skf=StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=0)
+        grid_obj = GridSearchCV(
+                       base_learner,
+                       param_grid = param_grid,
+                       n_jobs = 4,
+                       iid = True,
+                       cv=skf,
+                       refit = 'roc_auc',    # refit best model
+                       scoring=['roc_auc', 'f1','accuracy', 'recall', 'precision'])
+        grid_obj.fit(X, y)
+
+        best_clf = grid_obj.best_estimator_
+        cv_results = pd.DataFrame(grid_obj.cv_results_)
+
+        return best_clf, cv_results
+
+
+
+import seaborn as sns
+class ClassifierMetrics(object):
+
     @staticmethod
-    def classification_metrics(y_true, y_pred, y_pred_proba):
+    def classification_metrics(y_true, y_pred, y_pred_proba=None):
+        # report class for 1
+        accuracy = round(accuracy_score(y_true, y_pred), 4)
+        print("accuracy: ", accuracy)
 
-        roc_auc = roc_auc_score(y_true, y_pred_proba)
         recall = recall_score(y_true, y_pred)
+        print("recall: ", recall)
+
         precision = precision_score(y_true, y_pred)
+        print("precision: ", precision)
+
         f1 = f1_score(y_true, y_pred)
+        print("f1: ", f1)
 
-        print("roc_auc: ")
-        print(roc_auc)
-        print()
+        if y_pred_proba == None:
+            roc_auc = roc_auc_score(y_true, y_pred)
+        else:
+            roc_auc = roc_auc_score(y_true, y_pred_proba)
+        print("roc_auc: ", roc_auc)
 
-        print("classification_report: \n")
-        report = classification_report(y_true, y_pred)
-        print(report)
-        print()
+    #     print("classification_report: \n")
+    #     report = classification_report(y_true, y_pred)
+    #     print(report)
 
-
+    @staticmethod
+    def confusion_metrics(y_true, y_pred, normalized=False):
         labels = np.unique(y_true)
         index = ['true: {}'.format(idx) for idx in labels]
         column = ['pred: {}'.format(idx) for idx in labels]
 
         cm = confusion_matrix(y_true, y_pred, labels=labels)
         cm_df = pd.DataFrame(cm, columns = column, index = index)
+        cm_normalized_df = cm_df.div(cm_df.sum(axis=1), axis=0)*100
 
-        cm_normalized_df =  cm_df.div(cm_df.sum(axis=1), axis=0)
-        print("confusion_matrix: \n")
-        print(cm_df)
-        print("\n")
-        print("normalized_confusion_matrix: \n")
-        print(cm_normalized_df)
+        if normalized:
+            for i in range(cm_df.shape[0]):
+                for j in range(cm_df.shape[1]):
+                    cm_df.iloc[i,j] = "{} ({}%)".format(cm_df.iloc[i,j], round(cm_normalized_df.iloc[i,j], 1))
+        return cm_df
 
     @staticmethod
     def predict_proba_analysis(y_true, y_pred_proba, threshold = [0.3, 0.5, 0.8], plot_proba_violin = True, plot_thresohld_score=True):
@@ -236,3 +272,91 @@ class Classifier(object):
     @staticmethod
     def pred_with_threshold(pred_proba, threshold):
         return list(map(lambda x: 0 if x < threshold else 1, pred_proba))
+
+from sklearn.tree._tree import TREE_LEAF
+def ClassifierVisualize(object):
+    @staticmethod
+    def plot_roc_curve(y_true, y_pred, figsize, title = "Receiver operating characteristic example"):
+        # Compute ROC curve and ROC area for class 1
+        fpr,tpr,threshold = roc_curve(y_true, y_pred)
+        roc_auc = auc(fpr,tpr)
+        lw = 2
+
+        plt.figure(figsize=figsize)
+        plt.plot(fpr, tpr, color='darkorange',
+                 lw=lw, label='ROC curve (area = %0.2f)' % roc_auc, marker='.', markersize=7)
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title(title)
+        plt.legend(loc="lower right")
+        plt.show()
+
+    @staticmethod
+    def plot_precision_recall_curve(y_true, y_pred, figsize, title="Precision-Recall Curve"):
+        # https://machinelearningmastery.com/roc-curves-and-precision-recall-curves-for-classification-in-python/
+        # calculate precision-recall curve
+        precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
+
+        plt.plot([0, 1], [0.5, 0.5], linestyle='--')
+        # plot the precision-recall curve for the model
+        plt.plot(recall, precision, marker='.', markersize=7)
+        plt.xlim([-0.01, 1])
+        plt.ylim([-0.01, 1.05])
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        # show the plot
+        plt.show()
+
+
+    @staticmethod
+    def visualize_decision_tree(clf, feature_names, class_names, output_path="tree.png"):
+        # Create DOT data
+        dot_data = tree.export_graphviz(clf, out_file=None,
+                                        feature_names=feature_names,
+                                        class_names=class_names,
+                                        filled=True, rounded=True)
+        # Draw graph
+        graph = pydotplus.graph_from_dot_data(dot_data)
+        # Show graph
+        display(Image(graph.create_png()))
+
+        # # Create PDF
+        # graph.write_pdf("iris.pdf")
+        # Create PNG
+        graph.write_png(output_path)
+
+    """
+    Post=prune decision tree
+    """
+    @staticmethod
+    def is_leaf(inner_tree, index):
+        # Check whether node is leaf node
+        return (inner_tree.children_left[index] == TREE_LEAF and
+                inner_tree.children_right[index] == TREE_LEAF)
+    @staticmethod
+    def prune_index(inner_tree, decisions, index=0):
+        # Start pruning from the bottom - if we start from the top, we might miss
+        # nodes that become leaves during pruning.
+        # Do not use this directly - use prune_duplicate_leaves instead.
+        if not is_leaf(inner_tree, inner_tree.children_left[index]):
+            prune_index(inner_tree, decisions, inner_tree.children_left[index])
+        if not is_leaf(inner_tree, inner_tree.children_right[index]):
+            prune_index(inner_tree, decisions, inner_tree.children_right[index])
+
+        # Prune children if both children are leaves now and make the same decision:
+        if (is_leaf(inner_tree, inner_tree.children_left[index]) and
+            is_leaf(inner_tree, inner_tree.children_right[index]) and
+            (decisions[index] == decisions[inner_tree.children_left[index]]) and
+            (decisions[index] == decisions[inner_tree.children_right[index]])):
+            # turn node into a leaf by "unlinking" its children
+            inner_tree.children_left[index] = TREE_LEAF
+            inner_tree.children_right[index] = TREE_LEAF
+            ##print("Pruned {}".format(index))
+    @staticmethod
+    def prune_duplicate_leaves(mdl):
+        # Remove leaves if both
+        decisions = mdl.tree_.value.argmax(axis=2).flatten().tolist() # Decision for each node
+        prune_index(mdl.tree_, decisions)
